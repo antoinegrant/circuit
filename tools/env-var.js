19229a806164c5ba4a06eaf22153c302b9bcf116
Antoine Grant <antoine.grant@nytimes.com>
2016-09-23 23:04:06 -0400

    Remove tools, react-scripts provides these now. Added repository feild to package.json

--- tools/env-var.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-const yaml_config = require('node-yaml-config');
-
-const config = yaml_config.load(path.resolve(__dirname, '../config/app.yml'));
-
-module.exports = config;
